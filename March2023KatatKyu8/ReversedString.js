8d073424bcc8f66a0db0c529f50acbd3143cc902
Potato™ <donnyduong92@gmail.com>
2023-03-16 00:51:44 -0400

    Create ReversedString.js
    daily kata

---
+++ March2023KatatKyu8/ReversedString.js
@@ -0,0 +1,11 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+//Raw
+// function solution(str){
+//  return str.split('').reverse().join('')
+// }
+//Revamped
+const solution = s => s.split('').reverse().join('')
