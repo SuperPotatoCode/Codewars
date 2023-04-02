995c3b3be1091d5c516129d48dbd737a98a8632d
Potato™ <donnyduong92@gmail.com>
2023-04-02 11:39:33 -0400

    Create ConvertValuesToString.js

---
+++ April2023Katas/ConvertValuesToString.js
@@ -0,0 +1,10 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+//RAW
+// function boolToWord( bool ){
+//   return bool === true ? 'Yes' : 'No'
+// }
+
+//Revamped
+const boolToWord = b => b?'Yes':'No'
+
